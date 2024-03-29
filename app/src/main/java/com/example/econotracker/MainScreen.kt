bbd0b1ad5693e74dd69b31b6da9b3837d3121bc2
Abdul Saboor <abdulsaboor.bscssef20@iba-suk.edu.pk>
2024-03-29 14:44:55 +0500

    First Commit

--- app/src/main/java/com/example/econotracker/MainScreen.kt
+++ app/src/main/java/com/example/econotracker/MainScreen.kt
@@ -1,8 +1,47 @@
 package com.example.econotracker
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import java.nio.file.WatchEvent
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MainScreen(){
+fun MainScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        TopAppBar(
+            title = { Text(text = "Mian Screen") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Text(
+            text = "Current Balance: ",
+            fontSize =20.sp,
+            modifier = Modifier
+                .padding(20.dp)
+        )
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Add Income/Transaction")
+        }
+    }
+}
 
+@Preview (showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    MainScreen()
 }
