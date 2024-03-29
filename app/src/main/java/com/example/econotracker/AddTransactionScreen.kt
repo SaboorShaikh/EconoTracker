0ba4bf6abbd929e048952e397d165e8418b01ade
Abdul Saboor <abdulsaboor.bscssef20@iba-suk.edu.pk>
2024-03-29 14:59:21 +0500

    Second Commit

---
+++ app/src/main/java/com/example/econotracker/AddTransactionScreen.kt
@@ -0,0 +1,82 @@
+package com.example.econotracker
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material3.Button
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun AddTransactionScreen() {
+    var Title by remember {
+        mutableStateOf("")
+    }
+    var Ammount by remember {
+        mutableStateOf("")
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        TopAppBar(
+            title = { Text(text = "Add Transaction") }
+        )
+        Column {
+            OutlinedTextField(
+                value = Title,
+                onValueChange = { Title = it },
+                label = {
+                    Text(
+                        text = "Title",
+                        fontSize = 18.sp
+                    )
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp)
+            )
+            OutlinedTextField(
+                value = Ammount,
+                onValueChange = { Ammount = it },
+                label = {
+                    Text(
+                        text = "Ammount",
+                        fontSize = 18.sp
+                    )
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp)
+            )
+        }
+        Button(onClick = { /*TODO*/ }) {
+            Text(
+                text = "Add Transaction",
+                color = Color.White
+            )
+        }
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AddTransactionScreenPreview() {
+    AddTransactionScreen()
+}
